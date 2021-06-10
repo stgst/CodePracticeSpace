d72680b467dc97cb7a184c0f7c6b1d63108ad048
Steven Chan <steven19790906@gmail.com>
2021-06-10 16:21:38 +0800

    Update Survival.cpp

--- Survival.cpp
+++ Survival.cpp
@@ -6,8 +6,8 @@ int main()
     int N; //Total of Lions
     int H[2001]; //Limit of Lions is 2000
     int W[2001]; //Limit of Lions is 2000
-    int min = 1000001; //Maximum, Height*Weight =  1000000
-    int sum = 0; //Minimum
+    int max = 1000001; //Maximum, Height*Weight =  1000000
+    int min = 0; //Minimum
 
     cin >> N; //input Total of Lions
     for(int i=0;i<N;i++){
@@ -17,10 +17,10 @@ int main()
 
     for(int i=0;i<N;i++){
         cin >> W[i]; //input Weight of Lions
-        if(H[i]*W[i] < min){ //find Minimum of Height multiply Weight
-            min = H[i]*W[i];
-            sum = i;
+        if(H[i]*W[i] < max){ //find Minimum of Height multiply Weight
+            max = H[i]*W[i];
+            min = i;
         }
     }
-    cout << H[sum] << " " << W[sum];
+    cout << H[min] << " " << W[min];
 }
